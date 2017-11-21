f569a11e9ce18702ad6ed999ea8a28a5fb29cb17
root <root@localhost.localdomain>
2017-11-21 11:54:01 -0500

    v.0.9

--- server.py
+++ server.py
@@ -11,45 +11,23 @@ from client_manager import ClientManager
 class Server():
     def __init__(self,conn):
         self.conn = conn
-        self.sender = Sender(conn,settings.CC_EXCHANGE)
-        self.receiver = Receiver(conn,settings.CC_EXCHANGE,settings.CC_QUEUE)
-        
-        self.receiver.start()
-        self.client_manager = ClientManager(self.receiver, self.sender,keep_alive=100)
-
+        self.sender = Sender(conn, settings.LOCAL_EXCHANGE)
+        self.receiver = Receiver(conn, settings.LOCAL_EXCHANGE, settings.LOCAL_TASK_QUEUE)
         self.receiver.add_listener(self.on_result,["task_result"])
-        self.task_counter = 0
-        
+        self.receiver.start()
+
+        self.client_manager = ClientManager(self.receiver, self.sender,keep_alive=100)
+        self.task_queue = list()
     
-    def on_result(self, receiver, message):
-        print message
+    def on_result(self, receiver, method, body):
+        print body
+
+    def add_task(self, task):
+        self.task_queue.append(task)
+        if len(self.task_queue) > settings.MAX_QUEUE_LEN):
+            send_tasks(self.task_queue)
+            self.task_queue = list()
     
-    def send_tasks(self, payloads, reported_ids):
-        message = dict()
-        message["reported_ids"] = list()
-        message["tasks"] = dict()
-
-        reported_ids = set(reported_ids)
-        
-        for payload_id in xrange(0,len(payloads)):
-            message["tasks"][str(self.task_counter)] = payloads[payload_id]
-            if payload_id in reported_ids:
-                message["reported_ids"].append(self.task_counter)
-
-            self.task_counter += 1
-        
-        self.sender.send_message(routing_key="task",message=json.dumps(message))
-
-
-def main():
-    s = Server(conn)
-    s.start()
-    tasks = ["http://example.com","https://google.com"]
-    s.send_tasks(tasks,list(xrange(0,len(tasks))))
-
-if __name__== "__main__":
-    main()  
-          
-    
-
-    
+    def send_tasks(self, tasks):
+        self.sender.send_message(routing_key="task",message=json.dumps(tasks))
+      
